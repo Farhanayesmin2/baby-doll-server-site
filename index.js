94b14a006138a8c326a13feb142dec1296625f0b
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-05-19 15:59:31 +0600

    port running

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+
+// Get the data
+const babydoll = require('./babydoll.json');
+
+
+
+// Set running data or not
+app.get('/', (req, res) => {
+    res.send('api running in port 5000')
+})
+
+
+
+
+
+// this is the main port for run data
+app.listen(port, () => {
+    
+console.log('The port is runing on:',port);
+
+})
